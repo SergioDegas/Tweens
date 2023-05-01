bf01f03b123e2f51d6d74426bd298e9e270f1ee4
SergioDegas <laferces@gmail.com>
2023-05-01 23:21:02 +0300

    Update slice.js

--- src/Redax/Cards/slice.js
+++ src/Redax/Cards/slice.js
@@ -1,73 +1,70 @@
-import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { createSlice,  } from '@reduxjs/toolkit';
 import { fetchTweets } from './operations';
 
-
-const initialState = {
-  users: [],
-  follow: [],
-  isLoading: false,
-  error: null,
-};
-
-const extraActions = [fetchTweets];
-const getActions = type => isAnyOf(...extraActions.map(action => action[type]));
-
-const fetchUsersFulfilledReducer = (state, action) => {
-  const newUsers = action.payload.filter(
-    item => !state.users.some(existingItem => existingItem.id === item.id)
-  );
-  state.users = [...state.users, ...newUsers];
-};
-
-const contactsAnyPendingReducer = state => {
-  state.isLoading = true;
-};
-
-const contactsAnyFulfilledReducer = state => {
-  state.isLoading = false;
-  state.error = null;
-};
-
-const contactsAnyRejectedReducer = (state, action) => {
-  state.isLoading = false;
-  state.error = action.payload;
-};
-
-const toggleFollowingReducer = (state, action) => {
-  if (!state.follow.includes(action.payload)) {
-    state.follow.push(action.payload);
-    for (const user of state.users) {
-      if (user.id === action.payload) {
-        user.followers = Number(user.followers) + 1;
-        break;
-      }
-    }
-  } else {
-    const index = state.follow.findIndex(value => value === action.payload);
-    state.follow.splice(index, 1);
-    for (const user of state.users) {
-      if (user.id === action.payload) {
-        user.followers = Number(user.followers) - 1;
-        break;
-      }
-    }
-  }
-};
 
 
 const usersSlice = createSlice({
   name: 'users',
-  initialState,
-  reducers: {
-    toggleFollowing: toggleFollowingReducer,
+  initialState: {
+    users: [],
+    follow: [],
+    isLoading: false,
+    error: null,
   },
-  extraReducers: builder =>
+  reducers: {
+    toggleFollowing: (state, action) => {
+      const index = state.follow.indexOf(action.payload);
+      if (index === -1) {
+        state.follow.push(action.payload);
+      } else {
+        state.follow.splice(index, 1);
+      }
+
+      state.users = state.users.map(user => {
+        if (user.id === action.payload) {
+          return {
+            ...user,
+            followers:
+              index === -1
+                ? Number(user.followers) + 1
+                : Number(user.followers) - 1,
+          };
+        }
+        return user;
+      });
+    },
+  },
+  extraReducers: builder => {
     builder
-      .addCase(fetchTweets.fulfilled, fetchUsersFulfilledReducer)
-      .addMatcher(getActions('pending'), contactsAnyPendingReducer)
-      .addMatcher(getActions('rejected'), contactsAnyRejectedReducer)
-      .addMatcher(getActions('fulfilled'), contactsAnyFulfilledReducer),
+      .addCase(fetchTweets.fulfilled, (state, action) => {
+        const newUsers = action.payload.filter(
+          item => !state.users.some(existingItem => existingItem.id === item.id)
+        );
+        state.users = [...state.users, ...newUsers];
+      })
+      .addMatcher(
+        action => action.type.endsWith('/pending'),
+        state => {
+          state.isLoading = true;
+          state.error = null;
+        }
+      )
+      .addMatcher(
+        action => action.type.endsWith('/fulfilled'),
+        state => {
+          state.isLoading = false;
+          state.error = null;
+        }
+      )
+      .addMatcher(
+        action => action.type.endsWith('/rejected'),
+        (state, action) => {
+          state.isLoading = false;
+          state.error = action.error.message;
+        }
+      );
+  },
 });
 
 export const usersReducer = usersSlice.reducer;
-export const { resetUsers, toggleFollowing } = usersSlice.actions;
+export const { toggleFollowing } = usersSlice.actions;
